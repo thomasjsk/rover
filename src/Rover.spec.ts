d4901d99dcf932000757c3963f33f5b4378cb020
Thomasjsk <thomas.jasiuk@gmail.com>
2021-04-18 19:04:58 +0200

    Specified possible directions with an enum.

--- src/Rover.spec.ts
+++ src/Rover.spec.ts
@@ -6,14 +6,21 @@ class Rover {
   ) {}
 }
 
+enum Direction {
+  NORTH = 'NORTH',
+  SOUTH = 'SOUTH',
+  EAST = 'EAST',
+  WEST = 'WEST',
+}
+
 describe('Rover', () => {
   describe('land', () => {
     it('should land on given coordinate', () => {
-      const rover = new Rover(2, 5, 'NORTH');
+      const rover = new Rover(2, 5, Direction.NORTH);
 
       expect(rover.x).toEqual(2);
       expect(rover.y).toEqual(5);
-      expect(rover.direction).toEqual('NORTH');
+      expect(rover.direction).toEqual(Direction.NORTH);
     });
   });
 });
